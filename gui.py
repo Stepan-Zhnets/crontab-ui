b275dac92ecc026a9ab5621236155de94427e10b
Stepan Zhnets <stepan.zhnets@bk.ru>
2025-04-01 15:05:24 +0300

    update-1.04.2025-2

--- gui.py
+++ gui.py
@@ -1,40 +1,30 @@
 import flet as ft
 from components_gui.create_job import button_create_new_job
-from cron_tools import delete_job, get_jobs
+from cron_tools import delete_job#, get_jobs
 
 # Условные данные
-# list_jobs = [
-#     {
-#         "name": "Job 1",
-#         "command": "Command 1",
-#         "status":"stopped"
-#     },
-#     {
-#         "name": "Job 2",
-#         "command": "Command 2",
-#         "status":"started"
-#     },
-#     {
-#         "name": "Job 3",
-#         "command": "Command 3",
-#         "status":"stop"
-#     }
-# ]
+list_jobs = [
+    {
+        "name": "Job 1",
+        "command": "Command 1",
+        "status":"stopped"
+    },
+    {
+        "name": "Job 2",
+        "command": "Command 2",
+        "status":"started"
+    },
+    {
+        "name": "Job 3",
+        "command": "Command 3",
+        "status":"stop"
+    }
+]
 
 # Данные из CronTab
-list_jobs = get_jobs()
+# list_jobs = get_jobs()
 
-def delete_and_refresh(page, job_name):
-    delete_job(name=job_name)
-    page.update()
-
-# Главная страница
-def main(page: ft.Page):
-    page.title = "crontab ui"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-    button_create_new_job(page)
-
+def update_table(page):
     # Заголовки таблицы
     columns = [
         ft.DataColumn(ft.Text("#")),
@@ -47,7 +37,7 @@ def main(page: ft.Page):
     def delete(value):
         delete_job(name=value)
         # Обновление страницы
-        page.controls.append(data_table)
+        page.controls.append(rows)
         page.update()
 
     num=0
@@ -82,8 +72,21 @@ def main(page: ft.Page):
         ))
     data_table = ft.DataTable(columns=columns, rows=rows)
 
+    # Очищаем предыдущую таблицу и добавляем новую
+    page.clean()
     page.add(
-        ft.Container(content=data_table, padding=ft.padding.only(left=10, right=10))
+        ft.Container(content=data_table, padding=ft.padding.only(left=10, right=10)),
+        ft.TextButton(text="update page", on_click=lambda e: update_table(page))
     )
+
+# Главная страница
+def main(page: ft.Page):
+    page.title = "crontab ui"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    button_create_new_job(page)
+
+    # Инициализируем таблицу
+    update_table(page)
 
 ft.app(target=main)
