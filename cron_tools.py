ebdb0ddf0e7ccc4363c6a2070b4db35a5157e989
Stepan Zhnets <stepan.zhnets@bk.ru>
2025-04-02 14:51:03 +0300

    update-2.04.2025-3.6

--- cron_tools.py
+++ cron_tools.py
@@ -53,12 +53,17 @@ def start_and_stop_job(name, status):
     with CronTab(user=user) as cron:
         for job in cron:
             if job.comment == name:
-                if status.lower() == 'enabled':
-                    # Убираем комментарий, если задача должна быть активной.
-                    job.enable()
-                elif status.lower() == 'disabled':
-                    # Комментируем задачу, если она должна быть неактивной
-                    job.enable(False)
+                match status.lower():
+                    case 'enabled':
+                        job.enable(False)
+                    case 'disabled':
+                        job.enable()
+                # if status.lower() == 'enabled':
+                #     # Убираем комментарий, если задача должна быть активной.
+                #     job.enable()
+                # elif status.lower() == 'disabled':
+                #     # Комментируем задачу, если она должна быть неактивной
+                #     job.enable(False)
         cron.write()
     return None
 
