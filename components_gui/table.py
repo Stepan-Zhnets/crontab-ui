9728aa30b30da144b17021a62e6cc854c69df727
Stepan Zhnets <stepan.zhnets@bk.ru>
2025-05-06 11:51:26 +0300

    update-06.05.2025-1

--- components_gui/table.py
+++ components_gui/table.py
@@ -2,14 +2,14 @@ import flet as ft
 from components_gui.create_job import open_create_new_job
 from components_gui.edit_job import button_edit_job
 from components_gui.read_pdf import open_manual_list
-from cron_tools import delete_job, start_and_stop_job#, get_jobs
+from cron_tools import delete_job, start_and_stop_job, get_jobs
 
 # Условные данные
-list_jobs = []
+# list_jobs = []
 
 def update_table(page):
     # Данные из CronTab
-    # list_jobs = get_jobs()
+    list_jobs = get_jobs()
 
     def status_switch_button(e, name):
         e.control.selected = not e.control.selected
